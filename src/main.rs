46e002c8e4b2fcc175630bc7578f01aef6f12aa0
a-givertzman <a.givertzman@icloud.com>
2023-06-22 11:54:08 +0300

    sqlite, json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+use serde::{Serialize, Deserialize};
+use sqlite::Connection;
+
 fn main() {
     println!("Hello, world!");
+    // let path = ":memory";
+    let path = "./database.sqlite";
+    let connection = sqlite::open(path).unwrap();
+    // drop(&connection);
+    // create(&connection);
+    sel(&connection);
+}
+
+
+fn drop(connection: &Connection) {
+    let query = "
+        DROP TABLE IF EXISTS `users`;";
+    connection.execute(query).unwrap();
+
+}
+
+fn create(connection: &Connection) {
+    let query = "
+        CREATE TABLE `users` (
+            name TEXT, 
+            age INTEGER, 
+            created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+        );
+        INSERT INTO users(name, age) VALUES ('Alice', 42);
+        INSERT INTO users(name, age) VALUES ('Bob', 69);
+    ";
+    connection.execute(query).unwrap();
+
+}
+
+fn sel(connection: &Connection) {
+    let query = "SELECT * FROM `users`";
+    // let query = "SELECT * FROM users WHERE age > 50";
+    match connection.iterate(query, |pairs| {
+        println!("pair: {:?}", pairs);
+        for &(name, value) in pairs.iter() {
+            println!("{} = {}", name, value.unwrap());
+        }
+        true
+    }) {
+        Ok(_) => {},
+        Err(err) => {
+            println!("Error: {:?}", err);
+        },
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct SqlQuery {
+    pub lines: HashMap<String, DsLineConf>,
+
+}
+impl SqlQuery {
+    pub fn new(path: String) -> SqlQuery {
+        let configJson = fs::read_to_string(&path)
+            .expect(&format!("Error read file {}", path));
+        let lines: HashMap<String, DsLineConf> = serde_json::from_str(&configJson).unwrap();
+        DsConfig{lines}
+    }
 }
