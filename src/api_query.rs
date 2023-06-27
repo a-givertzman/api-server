f19b0ecbe392629373b1fbe8678e5ee16e5b8c8a
a-givertzman <a.givertzman@icloud.com>
2023-06-27 20:54:43 +0300

    ApiServer

--- src/api_query.rs
+++ src/api_query.rs
@@ -3,7 +3,7 @@
 use log::{debug, warn};
 use serde::{Serialize, Deserialize};
 
-use crate::api_query_type::ApiQueryType;
+use crate::{api_query_type::ApiQueryType, api_query_sql::ApiQuerySql};
 
 ///
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -14,28 +14,54 @@ pub struct ApiQuery {
 
 }
 impl ApiQuery {
+    ///
+    pub fn sql(jsonMap: &serde_json::Map<String, serde_json::Value>) -> Self {
+        ApiQuery {
+            auth_token: jsonMap["auth_token"].to_string(),
+            id: jsonMap["id"].to_string(),
+            query: ApiQueryType::Sql(ApiQuerySql {
+                database: jsonMap["sql"]["database"].to_string(),
+                sql: jsonMap["sql"]["sql"].to_string(),
+            }),
+        }
+    }
+    ///
     pub fn fromJson(jsonString: String) -> Self {
         let raw: ApiQuery = serde_json::from_str(&jsonString).unwrap();
         println!("raw: {:?}", raw);
         raw
     }
+    ///
     pub fn fromBytes(bytes: Vec<u8>) -> Self {
         let string = String::from_utf8(bytes).unwrap();
         let string = string.trim_matches(char::from(0));
-        debug!("[SqlQuery.fromBytes] string: {:#?}", string);
-        let query: ApiQuery = match serde_json::from_str(&string) {
-            Ok(value) => {value},
-            Err(err) => {
-                warn!("[SqlQuery.fromBytes] json conversion error: {:?}", err);
-                ApiQuery {
-                    auth_token: String::from("none"),
-                    id: String::from("0"),
-                    query: ApiQueryType::Error,
-                }
-            },
-        };
+        debug!("[SqlQuery.fromBytes] string: {:?}", string);
+        let mut json: serde_json::Value = serde_json::from_str(string).unwrap();
+        let obj = json.as_object().expect(format!("[SqlQuery.fromBytes] error parsing json: {:?}", string).as_str());
+        debug!("[SqlQuery.fromBytes] obj: {:?}", obj);
+        if obj.contains_key("sql") {
+            ApiQuery::sql(obj)
+        } else {
+            warn!("[SqlQuery.fromBytes] json conversion error in: {:?}", obj);
+            ApiQuery {
+                auth_token: String::from("none"),
+                id: String::from("0"),
+                query: ApiQueryType::Error,
+            }
+        }
+        // let query: ApiQuery = match serde_json::from_str(&string) {
+        //     Ok(value) => {value},
+        //     Err(err) => {
+        //         warn!("[SqlQuery.fromBytes] json conversion error: {:?}", err);
+        //         ApiQuery {
+        //             auth_token: String::from("none"),
+        //             id: String::from("0"),
+        //             query: ApiQueryType::Error,
+        //         }
+        //     },
+        // };
         // debug!("[DsPoint] point: {:#?}", point);
-        query
+        // query
     }
 
 }
--- src/api_query_type.rs
+++ src/api_query_type.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 
-use serde::{Serialize, Deserialize};
+use log::debug;
+use serde::{Serialize, Deserialize, Serializer, ser::SerializeStruct};
 
 use crate::api_query_sql::ApiQuerySql;
 
@@ -10,3 +11,18 @@ pub enum ApiQueryType {
     Error,
     Sql(ApiQuerySql),
 }
+
+// impl Serialize for ApiQueryType {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: Serializer, {
+//         // serializer.is_human_readable()
+//         // 3 is the number of fields in the struct.
+//         // debug!("ApiQueryType.serialize | state: {:?}", serializer);
+//         let mut state = serializer.serialize_struct("ApiQuerySql", 2)?;
+//         state.serialize_field("database", &self.database)?;
+//         // state.serialize_field("g", &self.g)?;
+//         // state.serialize_field("b", &self.b)?;
+//         state.end()
+//     }    
+// }
