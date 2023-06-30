e6138f61bf191396e975327c8ab4f6f1375fcbf6
a-givertzman <a.givertzman@icloud.com>
2023-06-30 23:49:32 +0300

    python scripts

---
+++ scripts/script_tamplate.py
@@ -0,0 +1,25 @@
+import sys
+import json
+
+# reading input params from stdin
+lines = sys.stdin.read()
+# parsing input params from the string
+parsed = json.loads(lines)
+
+
+# your code...
+# you can do some calculations
+# you cam use some data stored in the databases
+
+
+# build map to be returned to the frontend
+result = {
+    "aa": parsed['a'] * 2,
+    "bb": parsed['b'] * 2,
+}
+
+
+# returning data to the frontend
+print(
+    json.dumps(result)
+)
