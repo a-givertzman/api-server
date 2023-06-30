225078dc3893cc64f756a7e8e0d504fd54129c73
a-givertzman <a.givertzman@icloud.com>
2023-06-30 23:35:21 +0300

    python scripts

--- scripts/read_std_in.py
+++ scripts/read_std_in.py
@@ -5,14 +5,13 @@ for line in sys.stdin:
     if 'Exit' == line.rstrip():
         break
     parsed = json.loads(line)
+
+    # your code...
+
     result = {
-        "a": parsed['a'] * 2,
-        "b": parsed['b'] * 2,
+        "aa": parsed['a'] * 2,
+        "bb": parsed['b'] * 2,
     }
-    
-    # print(f'sys.stdin line: {line}')
-    # print(f'parsed json line: {parsed}')
-# print(result)
 print(
     json.dumps(result)
 )
