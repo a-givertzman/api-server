52e769e37918bc89ea3d94ef9f820ec65430f2e8
a-givertzman <a.givertzman@icloud.com>
2023-06-30 22:41:15 +0300

    python scripts

--- src/python_query.rs
+++ src/python_query.rs
@@ -30,27 +30,33 @@ impl PythonQuery {
     pub fn execute(&self) -> Result<Vec<RowMap>, String> {
         let path = self.path.clone();
         debug!("PythonQuery.execute | script: {:?}\n\twith params: {:?}", self.path, self.params);
-        let command = format!("python3 {}", path);
-        debug!("PythonQuery.execute | executing command: {:?}", command);
+        let program = "python3";
+        debug!("PythonQuery.execute | executing command: {:?}", program);
 
-        match Command::new(command).stdin(Stdio::piped())
+        match Command::new(program)
+                    .arg(path)
+                    .stdin(Stdio::piped())
                     .stderr(Stdio::piped())
                     .stdout(Stdio::piped())
                     .spawn() {
             Ok(mut child) => {
+                debug!("PythonQuery.execute | command child: {:?}", &child);
+                debug!("PythonQuery.execute | sending stdin data...");
                 child.stdin
                     .as_mut()
                     .ok_or("Child process stdin has not been captured!").unwrap()
                     .write_all(self.params.as_bytes()).unwrap();
                 let output = child.wait_with_output().unwrap();
+                debug!("PythonQuery.execute | command output: {:?}", output);
                 let result = if output.status.success() {
                     let mut result: Vec<RowMap> = vec![];
-                    let raw_output = String::from_utf8(output.stdout).unwrap();
-                    let words = raw_output.split_whitespace()
-                        .map(|s| s.to_lowercase())
-                        .collect::<HashSet<_>>();
-                    debug!("PythonQuery.execute | Found {} unique words:", words.len());
-                    debug!("PythonQuery.execute | {:?}", words);
+                    let rawOutput = String::from_utf8(output.stdout).unwrap();
+                    debug!("PythonQuery.execute | rawOutput: {:?}", rawOutput);
+                    // let words = raw_output.split_whitespace()
+                    //     .map(|s| s.to_lowercase())
+                    //     .collect::<HashSet<_>>();
+                    // debug!("PythonQuery.execute | Found {} unique words:", words.len());
+                    // debug!("PythonQuery.execute | {:?}", words);
                     Ok(result)
                 } else {
                     let err = String::from_utf8(output.stderr).unwrap();
