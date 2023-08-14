a761766f1f8d6a60726b9f9229daed612f6b5a59
a-givertzman <a.givertzman@icloud.com>
2023-08-14 17:47:52 +0300

    executable extension

---
+++ extensions/bin/request-to-bin.py
@@ -0,0 +1,20 @@
+import json
+import socket
+
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect(('127.0.0.1', 8899))
+
+obj = {
+    'a': 4,
+    'b': 7,
+}
+jsonStr = json.dumps(obj)
+
+clientSocket.sendall(jsonStr.encode('utf-8'))
+
+data = clientSocket.recv(4096)
+
+received = json.loads(data)
+
+print(f'received: {received}')
