946a70d251f35790e0c22931da245a704b79255e
a-givertzman <a.givertzman@icloud.com>
2023-08-25 13:22:40 +0300

    Request with wrong key Fixes

--- src/api_query_unknown.rs
+++ src/api_query_unknown.rs
@@ -9,27 +9,27 @@ pub struct ApiQueryUnknown {
     pub query: serde_json::Value,
 }
 impl ApiQueryUnknown {
-    pub fn fromJson(jsonString: String) -> Self {
-        let raw: ApiQueryUnknown = serde_json::from_str(&jsonString).unwrap();
-        println!("raw: {:?}", raw);
-        raw
-    }
-    pub fn fromBytes(bytes: Vec<u8>) -> Self {
-        let string = String::from_utf8(bytes).unwrap();
-        let string = string.trim_matches(char::from(0));
-        debug!("[ApiQueryUnknown.fromBytes] string: {:#?}", string);
-        let query: ApiQueryUnknown = match serde_json::from_str(&string) {
-            Ok(value) => {value},
-            Err(err) => {
-                warn!("[ApiQueryUnknown.fromBytes] json conversion error: {:?}", err);
-                ApiQueryUnknown {
-                    query: serde_json::Value::default(),
-                }
-            },
-        };
-        // debug!("[ApiQueryUnknown.fromBytes] bytes: {:?}", pobytesint);
-        query
-    }
+    // pub fn fromJson(jsonString: String) -> Self {
+    //     let raw: ApiQueryUnknown = serde_json::from_str(&jsonString).unwrap();
+    //     println!("raw: {:?}", raw);
+    //     raw
+    // }
+    // pub fn fromBytes(bytes: Vec<u8>) -> Self {
+    //     let string = String::from_utf8(bytes).unwrap();
+    //     let string = string.trim_matches(char::from(0));
+    //     debug!("[ApiQueryUnknown.fromBytes] string: {:#?}", string);
+    //     let query: ApiQueryUnknown = match serde_json::from_str(&string) {
+    //         Ok(value) => {value},
+    //         Err(err) => {
+    //             warn!("[ApiQueryUnknown.fromBytes] json conversion error: {:?}", err);
+    //             ApiQueryUnknown {
+    //                 query: serde_json::Value::default(),
+    //             }
+    //         },
+    //     };
+    //     // debug!("[ApiQueryUnknown.fromBytes] bytes: {:?}", pobytesint);
+    //     query
+    // }
     ///
     pub fn toString(self) -> String {
         format!("Query: {}", self.query)
