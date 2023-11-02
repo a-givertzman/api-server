52152c8e33552c1373ffe497199a4b01311a9482
a-givertzman <a.givertzman@icloud.com>
2023-11-02 16:23:13 +0300

    integration tests

--- tests/integration/run_tests.sh
+++ tests/integration/run_tests.sh
@@ -7,6 +7,8 @@ declare -a tests=(
     "tests/integration/test_sql_postgres.py"
 )
 
+cargo run --release
+
 for test in "${tests[@]}"; do
     if eval $app $test ; then
         echo "$test - succeeded"
