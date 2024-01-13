c517a6ebcf88c7e9a5eb2c3e3e32db9aa47c8db6
a-givertzman <a.givertzman@icloud.com>
2024-01-13 15:37:41 +0300

    sql formating

--- sql/create_postgres_database.sql
+++ sql/create_postgres_database.sql
@@ -65,11 +65,11 @@ begin
 	*/
     CREATE SEQUENCE event_uid_seq;
     create table if not exists event (
+        uid         decimal(24,12) not null default nextval('event_uid_seq'), --'1.000000000000', --COMMENT 'Идентификатор записи',
+        timestamp   timestamp not null,
         pid         int2 not null,                      --COMMENT 'Идентификатор тэга.',
         value       int2 not null,                      --COMMENT 'Значение тэга 0..255, аварийно если >0.',
-        uid         decimal(24,12) not null default nextval('event_uid_seq'), --'1.000000000000', --COMMENT 'Идентификатор записи',
         status      int2 not null,                      --COMMENT 'DsStatus as 0..255',
-        timestamp   timestamp not null,
         primary key (uid)
     ); 
     comment on table event is 'События. Только тэги дискретных значений, регистрируется 0 - как норма, > 0 - как авария.';
