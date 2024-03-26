4d36706720953cb8760fc907924bc53da259fcaa
Konstantin Antonov <knstantna@gmail.com>
2024-03-26 17:53:22 +0300

    fix db data

--- sql/ship/ship.sql
+++ sql/ship/ship.sql
@@ -19,18 +19,21 @@ CREATE TABLE if not exists ship (
 );
 
 INSERT INTO ship
-  (project_id, ship_id, key, value, value_type, name, unit)
+  (ship_id, key, value, value_type, name, unit)
 VALUES
-  (NULL, 1, 'name', 'M/V "YURIY ARSHENEVSKIY"', 'text', 'Name of ship', NULL),
-  (NULL, 1, 'navigation_area', 'R2', 'text', 'Type of navigation area', NULL),
-  (NULL, 1, 'water_density', '1.025', 'real', 'Water Density', 'g/ml'),
-  (NULL, 1, 'n_parts', '20', 'int', 'Number of Parts', NULL),
-  (NULL, 1, 'const_mass_shift_x', '1.05', 'real', 'Center of mass shift x', 'm'),
-  (NULL, 1, 'const_mass_shift_y', '0', 'real', 'Center of mass shift y', 'm'),
-  (NULL, 1, 'const_mass_shift_z', '5.32', 'real', 'Center of mass shift z', 'm'),
-  (NULL, 1, 'windage', '1089.79', 'real', 'Total windage area', 'm^2'),
-  (NULL, 1, 'windage_shift_x', '3.79', 'real', 'Center of mass shift x', 'm'),
-  (NULL, 1, 'windage_shift_z', '6.51', 'real', 'Center of mass shift y', 'm'),
-  (NULL, 1, 'windage_icing', '1193.58', 'real', 'Total windage area with icing', 'm^2'),
-  (NULL, 1, 'windage_icing_shift_x', '3.46', 'real', 'Center of mass shift x', 'm'),
-  (NULL, 1, 'windage_icing_shift_z', '7.03', 'real', 'Center of mass shift y', 'm');
+  (1, 'name', 'M/V "YURIY ARSHENEVSKIY"', 'text', 'Name of ship', NULL),
+  (1, 'navigation_area', 'R2', 'text', 'Type of navigation area', NULL),
+  (1, 'water_density', '1.025', 'real', 'Water Density', 'g/ml'),
+  (1, 'keel_area', '24.69', 'real', 'Keel area', 'm^2'),
+  (1, 'length', '118.39', 'real', 'Ship hull length', 'm'),
+  (1, 'breadth', '13.40', 'real', 'Ship hull width', 'm'),
+  (1, 'n_parts', '20', 'int', 'Number of Parts', NULL),
+  (1, 'const_mass_shift_x', '1.05', 'real', 'Center of mass shift x', 'm'),
+  (1, 'const_mass_shift_y', '0', 'real', 'Center of mass shift y', 'm'),
+  (1, 'const_mass_shift_z', '5.32', 'real', 'Center of mass shift z', 'm'),
+  (1, 'windage', '1089.79', 'real', 'Total windage area', 'm^2'),
+  (1, 'windage_shift_x', '3.79', 'real', 'Center of mass shift x', 'm'),
+  (1, 'windage_shift_z', '6.51', 'real', 'Center of mass shift y', 'm'),
+  (1, 'windage_icing', '1193.58', 'real', 'Total windage area with icing', 'm^2'),
+  (1, 'windage_icing_shift_x', '3.46', 'real', 'Center of mass shift x', 'm'),
+  (1, 'windage_icing_shift_z', '7.03', 'real', 'Center of mass shift y', 'm');
