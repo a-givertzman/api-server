521b0bf938316e973086b92c42e47529c3adbf76
Konstantin Antonov <knstantna@gmail.com>
2024-05-02 09:41:42 +0300

    Back | fix result_stability

--- sql/ship/result_stability.sql
+++ sql/ship/result_stability.sql
@@ -13,7 +13,7 @@ CREATE TABLE if not exists result_stability (
   CONSTRAINT result_stability_check_title CHECK(char_length(title) <= 100),
   CONSTRAINT result_stability_check_relationship CHECK(char_length(relationship) <= 50),
   CONSTRAINT result_stability_check_unit CHECK(char_length(unit) <= 50),
-  CONSTRAINT result_stability_check_comment CHECK(char_length(comment) <= 1000),
+  CONSTRAINT result_stability_check_comment CHECK(char_length(comment) <= 1000)
 );
 
 TRUNCATE TABLE result_stability;
@@ -21,7 +21,8 @@ TRUNCATE TABLE result_stability;
 INSERT INTO result_stability
   (title, value1, value2, relationship)
 VALUES
-  ('Критерий погоды K', 3.6, 1, '>=');
+  ('Критерий погоды K', 3.6, 1, '>='),
+  ('Критерий ускорения 𝐾∗', 35.0, 30, '>=');
 
 INSERT INTO result_stability
   (title, value1, value2, relationship, unit)
@@ -33,6 +34,5 @@ VALUES
   ('Макс. плечо DSO', 1.5, 0.25, '>=', 'm'),
   ('Угол соотв. макс. DSO', 35.0, 30, '>=', 'deg'),
   ('Исп. метацентрическая высота h', 0.5, 0.15, '>=', 'm'),
-  ('Критерий ускорения 𝐾∗', 35.0, '>=', 30),
   ('Крен на циркуляции', 35.0, 30, '>=', 'deg'),
   ('Смещение зерна, А', 0.1, 0.075, '>=', 'm*rad');
