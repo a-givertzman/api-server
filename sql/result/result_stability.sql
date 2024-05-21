fd2f972c17578e0a0dae16cd4dde38a2f5e23763
Konstantin Antonov <knstantna@gmail.com>
2024-05-21 18:17:12 +0300

    Back | fix result

--- sql/result/result_stability.sql
+++ sql/result/result_stability.sql
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS result_stability (
   project_id INT,
   ship_id INT NOT NULL,
   criterion_id INT NOT NULL,
-  value1 FLOAT8,
-  value2 FLOAT8,
+  result FLOAT8,
+  target FLOAT8,
   error_message TEXT,
   CONSTRAINT result_stability_pk PRIMARY KEY (id),
-  CONSTRAINT result_stability_id_unique UNIQUE (criterions_id),
-  CONSTRAINT result_stability_criterions_id_unique UNIQUE (project_id, ship_id, criterions_id),
+  CONSTRAINT result_stability_id_unique UNIQUE (criterion_id),
+  CONSTRAINT result_stability_criterions_id_unique UNIQUE (project_id, ship_id, criterion_id),
   CONSTRAINT result_stability_check_description CHECK(char_length(error_message) <= 1000)
 );
