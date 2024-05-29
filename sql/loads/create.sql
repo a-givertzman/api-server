ae580ebc41cb3d616c2f20e990e544d05adcca00
KnstAnt <knstantna@gmail.com>
2024-05-29 18:18:22 +0300

    Back | fix load_constant

--- sql/loads/create.sql
+++ sql/loads/create.sql
@@ -15,12 +15,11 @@ CREATE TABLE if not exists load_constant (
   id INT GENERATED ALWAYS AS IDENTITY,
   project_id INT,
   ship_id INT NOT NULL,
-  frame_space_index INT NOT NULL,
-  key TEXT NOT NULL,
-  value FLOAT8 NOT NULL,
+  frame_start_index INT NOT NULL,
+  frame_end_index INT NOT NULL,
+  mass FLOAT8 NOT NULL,
   CONSTRAINT load_constant_pk PRIMARY KEY (id),
-  CONSTRAINT load_constant_key_unique UNIQUE (ship_id, frame_space_index, key),
-  CONSTRAINT load_constant_key_check CHECK(char_length(key) > 0 AND char_length(key) <= 50)
+  CONSTRAINT load_space_bound_x_check CHECK(frame_start_index < frame_end_index)
 );
 
 -- Координаты и параметры отсеков и цистерн.
