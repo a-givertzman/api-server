aa22919e66125375afc641a54e8fd01709677d18
Konstantin Antonov <knstantna@gmail.com>
2024-05-30 17:16:02 +0300

    Back | fix ship_parameters

--- sql/ship/ship_parameters.sql
+++ sql/ship/ship_parameters.sql
@@ -9,7 +9,7 @@ CREATE TABLE if not exists ship_parameters (
   value_type TEXT NOT NULL,
   unit TEXT,
   CONSTRAINT ship_parameters_pk PRIMARY KEY (id),
-  CONSTRAINT ship_parameters_unique UNIQUE (ship_id, key),
+  CONSTRAINT ship_parameters_unique UNIQUE NULLS NOT DISTINCT (project_id, ship_id, key),
   CONSTRAINT ship_parameters_key_check CHECK(char_length(key) > 0 AND char_length(key) <= 50),
   CONSTRAINT ship_parameters_value_check CHECK(char_length(value) > 0 AND char_length(value) <= 50),
   CONSTRAINT ship_parameters_type_check CHECK(char_length(value_type) > 0 AND char_length(value_type) <= 10),
