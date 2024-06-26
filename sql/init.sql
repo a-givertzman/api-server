61e07a06f0a4536c06b7cfc745adc259b59a8720
KnstAnt <knstantna@gmail.com>
2024-06-26 10:19:30 +0300

    Back | add open_deck_angle

--- sql/init.sql
+++ sql/init.sql
@@ -7,6 +7,7 @@
 \i ./sql/ship/rad_trans.sql
 \i ./sql/ship/rad_long.sql
 \i ./sql/ship/flooding_angle.sql
+\i ./sql/ship/open_deck_angle.sql
 \i ./sql/ship/entry_angle.sql
 \i ./sql/ship/volume_shift.sql
 \i ./sql/ship/waterline_breadth.sql
