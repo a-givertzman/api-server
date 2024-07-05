1c0e415524f21122c24405bfb31b493a07eb95db
KnstAnt <knstantna@gmail.com>
2024-07-05 08:38:05 +0300

    Back | fix draft_mark

--- sql/frames/draft_mark.sql
+++ sql/frames/draft_mark.sql
@@ -8,9 +8,9 @@ CREATE TABLE if not exists draft_mark (
   project_id INT,
   ship_id INT NOT NULL,
   name TEXT NOT NULL,
-  z FLOAT8 NOT NULL,
   x FLOAT8 NOT NULL,
   y FLOAT8 NOT NULL,
+  z FLOAT8 NOT NULL,
   CONSTRAINT draft_mark_pk PRIMARY KEY (id),
   CONSTRAINT draft_mark_name_check CHECK(char_length(name) > 0 AND char_length(name) <= 50),
   CONSTRAINT draft_mark_name_unique UNIQUE NULLS NOT DISTINCT (project_id, ship_id, name, z)
