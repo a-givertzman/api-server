a8c4097a0532fbf3090d94ff48c86df2000ebf63
KnstAnt <knstantna@gmail.com>
2024-09-09 17:08:36 +0300

    Back | fix compartment trigger

--- sql/loads/loads_trigger.sql
+++ sql/loads/loads_trigger.sql
@@ -66,7 +66,7 @@ BEGIN
     -- Check if new category is of bulk type and then update grain_moment for compartment entry;
     SELECT cc.matter_type INTO matter_type FROM cargo_category AS cc WHERE id = NEW.category_id;
     IF (matter_type = 'bulk') THEN
-        NEW.grain_moment = get_grain_moment_level(NEW.ship_id, NEW.space_id, NEW.level);
+        NEW.grain_moment = get_grain_moment(NEW.ship_id, NEW.space_id, NEW.level);
     END IF;
 
     RAISE NOTICE 'update_compartment_parameters OK, NEW:[%]', NEW;
@@ -141,20 +141,19 @@ END;
 $get_compartment_curve_volume$ LANGUAGE plpgsql;
 
 
-DROP FUNCTION IF EXISTS get_grain_moment_level CASCADE;
-CREATE OR REPLACE FUNCTION get_grain_moment_level(src_ship_id INT, src_space_id INT, src_level FLOAT8) 
+DROP FUNCTION IF EXISTS get_grain_moment CASCADE;
+CREATE OR REPLACE FUNCTION get_grain_moment(src_ship_id INT, src_space_id INT, src_level FLOAT8) 
 RETURNS FLOAT8 
-AS $get_grain_moment_level$
+AS $get_grain_moment$
 DECLARE 
     r1 grain_moment%rowtype;
     r2 grain_moment%rowtype;
-    res_level FLOAT8;
     res_moment FLOAT8;
     delta FLOAT8;
     coeff1 FLOAT8;
     coeff2 FLOAT8;
 BEGIN 
-    RAISE NOTICE 'get_grain_moment_level ship_id:[%] space_id:[%] level:[%]', src_ship_id, src_space_id, src_level;
+    RAISE NOTICE 'get_grain_moment ship_id:[%] space_id:[%] level:[%]', src_ship_id, src_space_id, src_level;
 
     SELECT 
         *
@@ -172,7 +171,7 @@ BEGIN
     WHERE ship_id = src_ship_id AND space_id = src_space_id
     ORDER BY ABS(src_level - t.level) ASC LIMIT 1 OFFSET 1;
 
-    RAISE NOTICE 'get_grain_moment_level res r1 level:[%] moment:[%]  r2 level:[%] moment:[%] ', r1.level, r1.moment, r2.level, r2.moment;
+    RAISE NOTICE 'get_grain_moment res r1 level:[%] moment:[%]  r2 level:[%] moment:[%] ', r1.level, r1.moment, r2.level, r2.moment;
 
 
     IF (r1.level < src_level AND r2.level < r1.level) THEN
@@ -192,14 +191,13 @@ BEGIN
         coeff2 = (src_level - r2.level) / delta;
     END IF;
 
-    res_level = r2.level*coeff1 + r1.level*coeff2;  
     res_moment = r2.moment*coeff1 + r1.moment*coeff2;
     
-    RAISE NOTICE 'get_grain_moment_level OK, src_level:[%] res_level:[%] moment:[%]', src_level, res.level, res_moment;
+    RAISE NOTICE 'get_grain_moment OK, src_level:[%] res_moment:[%]', src_level, res_moment;
 
-    RETURN res;
+    RETURN res_moment;
 END;
-$get_grain_moment_level$ LANGUAGE plpgsql;
+$get_grain_moment$ LANGUAGE plpgsql;
 
 
 DROP FUNCTION IF EXISTS get_compartment_curve_level CASCADE;
