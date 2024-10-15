79cb0de42b1979a4d900636fa179964effa3ff6f
KnstAnt <knstantna@gmail.com>
2024-10-15 17:36:50 +0300

    Back | fix load_line

--- sql/ARK/draft/load_line.sql
+++ sql/ARK/draft/load_line.sql
@@ -4,6 +4,6 @@ INSERT INTO load_line
   (ship_id, name, x, y, z)
 VALUES
   (1, 'LL draft S PS', 0,  -6.7, 4.6),
-  (1, 'LL draft S SB', 0, 6.7, 4.708),
+  (1, 'LL draft S SB', 0, 6.7, 4.6),
   (1, 'LL draft F PS', 0.425, -6.7, 4.708),
   (1, 'LL draft F SB', 0.425, 6.7, 4.708);
