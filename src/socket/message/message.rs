5096e5e37aff70c442120df6e4012aa6f8f618c6
Anton Lobanov <a.givertzman@icloud.com>
2024-10-21 14:35:54 +0300

    Socket Message | Initial

---
+++ src/socket/message/message.rs
@@ -0,0 +1,32 @@
+//!
+//! Messages transmitted over socket.
+//! - Data can be encoded using varius data `Kind`, `Size` and payload Data
+//! - Message format
+//!     Field name | Start | Kind |  Size  | Data |
+//!     ---       |  ---  | ---  |  ---   | ---  |
+//!     Data type |  u8   | u8   | u32    | [u8; Size] |
+//!     Value     |  22   | StringValue | xxx    | [..., ...]  |
+//!     
+//!     - Start - Each message starts with SYN (22)
+//!     - Kind - The `Kind` of the data stored in the `Data` field, refer to
+//!     - Size - The length of the `Data` field in bytes
+//!     - Data - Data structured depending on it `Kind`
+//! 
+//! - `Kind` of data
+//!     - 00, Any
+//!     - 01, Empty
+//!     - 02, Bytes
+//!     - 08, Bool
+//!     - 16, UInt16
+//!     - 17, UInt32
+//!     - 18, UInt64
+//!     - 24, Int16
+//!     - 25, Int32
+//!     - 26, Int64
+//!     - 32, F32
+//!     - 33, F64
+//!     - 40, String
+//!     - 48, Timestamp
+//!     - 49, Duration
+//!     - .., ...
+//! 
