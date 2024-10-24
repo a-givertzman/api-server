7e02484bd3edc6afa9dccac1018d5b3d7845b5d6
KnstAnt <knstantna@gmail.com>
2024-10-24 15:34:19 +0300

    Back | fix hydrostatic curves

--- sql/hidrostatic/center_draught.sql
+++ sql/hidrostatic/center_draught.sql
@@ -1,13 +1,14 @@
 -- Отстояние центра величины погруженной части судна в
--- зависимости от объемного водоизмещения
--- V, м3 | Xc, м | Yc, м, | Zc, м
+-- зависимости от дифферента и объемного водоизмещения
+-- Trim, m | V, м3 | Xc, м | Yc, м, | Zc, м
 DROP TABLE IF EXISTS center_draught CASCADE;
 
 CREATE TABLE if not exists center_draught (
   id INT GENERATED ALWAYS AS IDENTITY,
   project_id INT,
   ship_id INT NOT NULL,
-  key FLOAT8 NOT NULL,
+  trim FLOAT8 NOT NULL DEFAULT 0,  
+  volume FLOAT8 NOT NULL,
   value_x FLOAT8 NOT NULL,
   value_y FLOAT8 NOT NULL,
   value_z FLOAT8 NOT NULL,
--- sql/hidrostatic/waterline_length.sql
+++ sql/hidrostatic/waterline_length.sql
@@ -1,16 +1,15 @@
 -- Длинна судна по ватерлинии
--- в зависимости от дифферента и осадки
--- Trim, m | d, м | z, м
+-- в зависимости от осадки
+-- d, м | z, м
 DROP TABLE IF EXISTS waterline_length CASCADE;
 
 CREATE TABLE if not exists waterline_length (
   id INT GENERATED ALWAYS AS IDENTITY,
   project_id INT,
   ship_id INT NOT NULL,
-  trim FLOAT8 NOT NULL DEFAULT 0,  
-  draught FLOAT8 NOT NULL,
+  key FLOAT8 NOT NULL,
   value FLOAT8 NOT NULL,
   CONSTRAINT waterline_length_pk PRIMARY KEY (id),
-  CONSTRAINT waterline_length_unique UNIQUE NULLS NOT DISTINCT (project_id, ship_id, trim, draught),
-  CONSTRAINT waterline_length_draught_check CHECK(draught >= 0)
+  CONSTRAINT waterline_length_unique UNIQUE NULLS NOT DISTINCT (project_id, ship_id, key),
+  CONSTRAINT waterline_length_key_check CHECK(key >= 0)
 );
