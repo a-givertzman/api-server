0c4f9771513eb28cc3a3f56473c21d2839a2139b
nyaneet <nyaneet@gmail.com>
2024-11-26 20:49:04 +0300

    ARK | add fake data for voyage

--- sql/Sofia/voyage/voyage_insert.sql
+++ sql/Sofia/voyage/voyage_insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO voyage
     (ship_id, project_id, name, code, description)
 VALUES
-    (2, NULL, 'Voyage name', 'Voyage code', 'Voyage description');
+    (2, NULL, 'Voyage name', 'Voyage code', 'Voyage description'); -- TODO replace with initial data
