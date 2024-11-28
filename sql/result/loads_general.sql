5303a4b8f52c8a984236a7b63662647d992f946f
nyaneet <nyaneet@gmail.com>
2024-11-28 17:37:55 +0300

    loads_general | add project_id field

--- sql/result/loads_general.sql
+++ sql/result/loads_general.sql
@@ -3,6 +3,7 @@ DROP VIEW IF EXISTS loads_general;
 
 CREATE VIEW loads_general AS SELECT
     cargo.ship_id,
+    cargo.project_id,
     COALESCE(cargo.mass, 0.0) AS cargo,
     COALESCE(ballast.mass, 0.0) AS ballast,
     COALESCE(store.mass, 0.0) AS store,
@@ -12,26 +13,40 @@ CREATE VIEW loads_general AS SELECT
     COALESCE(displacement.mass, 0.0) AS displacement,
     COALESCE(deadweight.mass, 0.0) AS deadweight
 FROM (
-    SELECT ship_id, result AS "mass" FROM parameter_data WHERE parameter_id = 27
+    SELECT ship_id, project_id, result AS "mass" FROM parameter_data WHERE parameter_id = 27
 ) AS cargo
 FULL OUTER JOIN (
-    SELECT ship_id, result AS "mass" FROM parameter_data WHERE parameter_id = 25
-) AS ballast ON cargo.ship_id = ballast.ship_id
+    SELECT ship_id, project_id, result AS "mass" FROM parameter_data WHERE parameter_id = 25
+) AS ballast ON
+    cargo.ship_id = ballast.ship_id AND
+    cargo.project_id IS NOT DISTINCT FROM ballast.project_id
 FULL OUTER JOIN (
-    SELECT ship_id, result AS "mass" FROM parameter_data WHERE parameter_id = 26
-) AS store ON cargo.ship_id = store.ship_id
+    SELECT ship_id, project_id, result AS "mass" FROM parameter_data WHERE parameter_id = 26
+) AS store ON
+    cargo.ship_id = store.ship_id AND
+    cargo.project_id IS NOT DISTINCT FROM store.project_id
 FULL OUTER JOIN (
-    SELECT ship_id, result AS "mass" FROM parameter_data WHERE parameter_id = 29
-) AS lightship ON cargo.ship_id = lightship.ship_id
+    SELECT ship_id, project_id, result AS "mass" FROM parameter_data WHERE parameter_id = 29
+) AS lightship ON
+    cargo.ship_id = lightship.ship_id AND
+    cargo.project_id IS NOT DISTINCT FROM lightship.project_id
 FULL OUTER JOIN (
-    SELECT ship_id, result AS "mass" FROM parameter_data WHERE parameter_id = 2
-) AS displacement ON cargo.ship_id = displacement.ship_id
+    SELECT ship_id, project_id, result AS "mass" FROM parameter_data WHERE parameter_id = 2
+) AS displacement ON
+    cargo.ship_id = displacement.ship_id AND
+    cargo.project_id IS NOT DISTINCT FROM displacement.project_id
 FULL OUTER JOIN (
-    SELECT ship_id, result AS "mass" FROM parameter_data WHERE parameter_id = 28
-) AS deadweight ON cargo.ship_id = deadweight.ship_id
+    SELECT ship_id, project_id, result AS "mass" FROM parameter_data WHERE parameter_id = 28
+) AS deadweight ON
+    cargo.ship_id = deadweight.ship_id AND
+    cargo.project_id IS NOT DISTINCT FROM deadweight.project_id
 FULL OUTER JOIN (
-    SELECT ship_id, result AS "mass" FROM parameter_data WHERE parameter_id = 30
-) AS icing ON cargo.ship_id = icing.ship_id
+    SELECT ship_id, project_id, result AS "mass" FROM parameter_data WHERE parameter_id = 30
+) AS icing ON
+    cargo.ship_id = icing.ship_id AND
+    cargo.project_id IS NOT DISTINCT FROM icing.project_id
 FULL OUTER JOIN (
-    SELECT ship_id, result AS "mass" FROM parameter_data WHERE parameter_id = 31
-) AS wetting ON cargo.ship_id = wetting.ship_id;
+    SELECT ship_id, project_id, result AS "mass" FROM parameter_data WHERE parameter_id = 31
+) AS wetting ON
+    cargo.ship_id = wetting.ship_id AND
+    cargo.project_id IS NOT DISTINCT FROM wetting.project_id;
