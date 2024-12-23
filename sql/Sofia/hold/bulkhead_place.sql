ec7d5857af3dfe52f235d475008a8db7495b8bff
KnstAnt <knstantna@gmail.com>
2024-12-23 10:30:56 +0300

    Back | change default num of bounds to 200, change data for sofia

--- sql/Sofia/hold/bulkhead_place.sql
+++ sql/Sofia/hold/bulkhead_place.sql
@@ -3,15 +3,15 @@ DELETE FROM bulkhead_place WHERE ship_id=2;
 INSERT INTO bulkhead_place
   (ship_id, code, name_rus, name_engl, hold_group_id, bound_x1, bound_x2, mass_shift_x, mass_shift_y, mass_shift_z)
 VALUES
-  (2, 'SP101', 'Трюм 1.  По-походному ', 'Hold 1.Stowage Position', (SELECT id FROM hold_group WHERE space_id=1001), 73.358, 74.673, 74.016, 0.000, 6.530),
-  (2, 'SP102', 'Трюм 1.  По-походному ', 'Hold 1.Stowage Position', (SELECT id FROM hold_group WHERE space_id=1001), 74.673, 75.988, 75.331, 0.000, 6.530),
-  (2, 'P101', 'Трюм 1. Шп. 105 ', 'Hold 1. Fr. 105 ', (SELECT id FROM hold_group WHERE space_id=1001), 105.180, 105.920, 105.50, 0.00, 6.502),
-  (2, 'P102', 'Трюм 1. Шп. 97', 'Hold 1. Fr. 97', (SELECT id FROM hold_group WHERE space_id=1001), 96.300, 97.040, 96.88, 0.00, 6.502),
-  (2, 'P103', 'Трюм 1. Шп. 93', 'Hold 1. Fr. 93', (SELECT id FROM hold_group WHERE space_id=1001), 92.600, 93.340, 92.92, 0.00, 6.502),
-  (2, 'SP201', 'Трюм №2.  По-походному ', 'Hold 2.Stowage Position', (SELECT id FROM hold_group WHERE space_id=1002), 18.600, 19.91, 19.257, 0.00, 6.530),
-  (2, 'SP202', 'Трюм №2.  По-походному ', 'Hold 2.Stowage Position', (SELECT id FROM hold_group WHERE space_id=1002), 19.91, 21.230, 20.572, 0.00, 6.530),
-  (2, 'P201', 'Трюм 2. Шп. 58', 'Hold 2. Fr. 58', (SELECT id FROM hold_group WHERE space_id=1002), 57.820, 58.560, 58.08, 0.00, 6.502),
-  (2, 'P202', 'Трюм 2. Шп. 54', 'Hold 2. Fr. 54', (SELECT id FROM hold_group WHERE space_id=1002), 54.120, 54.860, 54.28, 0.00, 6.502),
-  (2, 'P203', 'Трюм 2. Шп. 42', 'Hold 2. Fr. 42', (SELECT id FROM hold_group WHERE space_id=1002), 41.540, 42.280, 41.86, 0.00, 6.502),
-  (2, 'P204', 'Трюм 2. Шп. 31', 'Hold 2. Fr. 31', (SELECT id FROM hold_group WHERE space_id=1002), 31.180, 31.920, 31.50, 0.00, 6.502),
-  (2, 'P205', 'Трюм 2. Шп. 26', 'Hold 2. Fr. 26', (SELECT id FROM hold_group WHERE space_id=1002), 25.260, 26.000, 25.58, 0.00, 6.502);
+  (2, 'SP101', 'Трюм 1.  По-походному ', 'Hold 1.Stowage Position', (SELECT id FROM hold_group WHERE space_id=1001), 73.358, 74.673, 74.0155, 0, 6.53),
+  (2, 'SP102', 'Трюм 1.  По-походному ', 'Hold 1.Stowage Position', (SELECT id FROM hold_group WHERE space_id=1001), 74.673, 75.988, 75.3305, 0, 6.53),
+  (2, 'P101', 'Трюм 1. Шп. 136-137', 'Hold 1. Fr. 136-137', (SELECT id FROM hold_group WHERE space_id=1001), 105.18, 105.92, 105.5, 0, 6.502),
+  (2, 'P102', 'Трюм 1. Шп. 124-125', 'Hold 1. Fr. 124-125', (SELECT id FROM hold_group WHERE space_id=1001), 96.3, 97.04, 96.883, 0, 6.502),
+  (2, 'P103', 'Трюм 1. Шп. 119-120', 'Hold 1. Fr. 119-120', (SELECT id FROM hold_group WHERE space_id=1001), 92.6, 93.34, 92.92, 0, 6.502),
+  (2, 'SP201', 'Трюм №2.  По-походному ', 'Hold 2.Stowage Position', (SELECT id FROM hold_group WHERE space_id=1002), 18.6, 19.913, 19.256500000000003, 0, 6.53),
+  (2, 'SP202', 'Трюм №2.  По-походному ', 'Hold 2.Stowage Position', (SELECT id FROM hold_group WHERE space_id=1002), 19.913, 21.23, 20.5715, 0, 6.53),
+  (2, 'P201', 'Трюм 2. Шп. 81-82', 'Hold 2. Fr. 81-82', (SELECT id FROM hold_group WHERE space_id=1002), 57.82, 58.56, 58.084, 0, 6.502),
+  (2, 'P202', 'Трюм 2. Шп. 76-77', 'Hold 2. Fr. 76-77', (SELECT id FROM hold_group WHERE space_id=1002), 54.12, 54.86, 54.276, 0, 6.502),
+  (2, 'P203', 'Трюм 2. Шп. 59-60', 'Hold 2. Fr. 59-60', (SELECT id FROM hold_group WHERE space_id=1002), 41.54, 42.28, 41.86, 0, 6.502),
+  (2, 'P204', 'Трюм 2. Шп. 45-46', 'Hold 2. Fr. 45-46', (SELECT id FROM hold_group WHERE space_id=1002), 31.18, 31.92, 31.5, 0, 6.502),
+  (2, 'P205', 'Трюм 2. Шп. 37-38', 'Hold 2. Fr. 37-38', (SELECT id FROM hold_group WHERE space_id=1002), 25.26, 26, 25.58, 0, 6.502);
--- sql/_Sofia/draft/bow_board.sql
+++ sql/_Sofia/draft/bow_board.sql
@@ -1,7 +1,7 @@
-DELETE FROM bow_board WHERE ship_id=2;
+-- Высота борта на носовом перпендикуляре
 
 INSERT INTO bow_board
-  (ship_id, criterion_id, name_rus, name_engl, x, y, z)
+  (ship_id, criterion_id, name, x, y, z)
 VALUES
-  (2, 143, ' Высота на носовом перпендикуляре ПрБ', 'Depth at forward perpendicular SB', 130.5, 4.555, 12.63),
-  (2, 144, ' Высота на носовом перпендикуляре ЛБ', 'Depth at forward perpendicular PS', 130.5, -4.555, 12.63);
+  (2, 143, 'Board on forward perpendicular SB', 130.500,	4.555, 12.630),
+  (2, 144, 'Board on forward perpendicular PS', 130.500,	-4.555, 12.630);
