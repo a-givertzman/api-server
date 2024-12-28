940b43026d86873469c01cbf520bb671ca0dc12e
nyaneet <nyaneet@gmail.com>
2024-12-28 16:22:27 +0300

    Sofia | remove extra tests from checkpoints

--- sql/init.sql
+++ sql/init.sql
@@ -231,30 +231,30 @@ VALUES
     ('Test 4', 1, FALSE);
 UPDATE custom_metadata.db_checkpoint SET is_active = TRUE WHERE name = 'Empty';
 
--- Test 5
+-- Test 5, as default loading
 \i ./sql/Sofia/test/SSS_Sofia_test5.sql
-INSERT INTO
-    custom_metadata.db_checkpoint (name, database_info_id, is_deletable)
-VALUES
-    ('Test 5', 1, FALSE);
-UPDATE custom_metadata.db_checkpoint SET is_active = TRUE WHERE name = 'Empty';
+-- INSERT INTO
+--     custom_metadata.db_checkpoint (name, database_info_id, is_deletable)
+-- VALUES
+--     ('Test 5', 1, FALSE);
+-- UPDATE custom_metadata.db_checkpoint SET is_active = TRUE WHERE name = 'Empty';
 
--- Test 6
-\i ./sql/Sofia/test/SSS_Sofia_test6.sql
-INSERT INTO
-    custom_metadata.db_checkpoint (name, database_info_id, is_deletable)
-VALUES
-    ('Test 6', 1, FALSE);
-UPDATE custom_metadata.db_checkpoint SET is_active = TRUE WHERE name = 'Empty';
+-- -- Test 6
+-- \i ./sql/Sofia/test/SSS_Sofia_test6.sql
+-- INSERT INTO
+--     custom_metadata.db_checkpoint (name, database_info_id, is_deletable)
+-- VALUES
+--     ('Test 6', 1, FALSE);
+-- UPDATE custom_metadata.db_checkpoint SET is_active = TRUE WHERE name = 'Empty';
 
--- Test 7
-\i ./sql/Sofia/test/SSS_Sofia_test7.sql
-INSERT INTO
-    custom_metadata.db_checkpoint (name, database_info_id, is_deletable)
-VALUES
-    ('Test 7', 1, FALSE);
-UPDATE custom_metadata.db_checkpoint SET is_active = TRUE WHERE name = 'Empty';
+-- -- Test 7
+-- \i ./sql/Sofia/test/SSS_Sofia_test7.sql
+-- INSERT INTO
+--     custom_metadata.db_checkpoint (name, database_info_id, is_deletable)
+-- VALUES
+--     ('Test 7', 1, FALSE);
+-- UPDATE custom_metadata.db_checkpoint SET is_active = TRUE WHERE name = 'Empty';
 
 
 -- Load default data for Sofia
-UPDATE custom_metadata.db_checkpoint SET is_active = TRUE WHERE name = 'Test 5';
+-- UPDATE custom_metadata.db_checkpoint SET is_active = TRUE WHERE name = 'Test 5';
