0125f1a6aedb5946590036fd4c48e0bec078d549
nyaneet <nyaneet@gmail.com>
2025-08-27 20:09:26 +0300

    DB, Seakeeping | add doc comments for seakeeping_zones

--- sql/result/seakeeping_zones.sql
+++ sql/result/seakeeping_zones.sql
@@ -1,10 +1,10 @@
 CREATE TYPE IF NOT EXISTS zone_type AS ENUM ('Parametric', 'Main', 'Broaching', 'HighWaves');
 
+-- Stores points for seakeeping zones
 CREATE TABLE IF NOT EXISTS seakeeping_zones (
-    id SERIAL PRIMARY KEY,
-    angle FLOAT NOT NULL,
-    speed FLOAT NOT NULL,
-    zone_id zone_type NOT NULL,
-    
+    id SERIAL PRIMARY KEY, -- Unique identifier of this point 
+    angle FLOAT NOT NULL, -- Angle of this point, measured in degrees
+    speed FLOAT NOT NULL, -- Speed of this point, measured in knots
+    zone_id zone_type NOT NULL, -- Determines which zone this point belongs to
     CONSTRAINT unique_angle_speed_zone UNIQUE (angle, speed, zone_id)
 );
