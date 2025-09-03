e64ce7ca087dc52ff658cd289fa9448fee745fc0
pivovarvalera <pivovarvaleri@gmail.com>
2025-09-03 19:45:48 +0300

    DB, Seakeeping | add parameters

--- sql/voyage/voyage_create.sql
+++ sql/voyage/voyage_create.sql
@@ -15,8 +15,10 @@ CREATE TABLE IF NOT EXISTS voyage (
     icing_timber_type_id INT NOT NULL DEFAULT 1,
     water_area_id INT NOT NULL DEFAULT 1,
     load_line_id INT NOT NULL DEFAULT 1,
-    wave_length FLOAT8 NOT NULL DEFAULT 5.0, -- Length of wave, m
-    course_angle FLOAT8 NOT NULL DEFAULT 1.0, -- Angle of vessel course in northeastern coordinate's, degrees
+    wave_length FLOAT8 NOT NULL DEFAULT 56.16, -- Length of wave, m
+    course_angle FLOAT8 NOT NULL DEFAULT 270.0, -- Angle of vessel course in northeastern coordinate's, degrees
+    wave_angle FLOAT8 NOT NULL DEFAULT 1.0, -- Wave heading angle in northeastern coordinate's, degrees // betta
+    current_speed FLOAT8 NOT NULL DEFAULT 1.0, -- Current speed of vessel, m
     CONSTRAINT voyage_pk PRIMARY KEY (id),
     CONSTRAINT voyage_name_check CHECK(char_length(name) > 0 AND char_length(name) <= 250),
     CONSTRAINT voyage_description_check CHECK(char_length(description) <= 1000),
@@ -26,5 +28,7 @@ CREATE TABLE IF NOT EXISTS voyage (
     CONSTRAINT voyage_type_water_area_type_fk FOREIGN KEY (water_area_id) REFERENCES ship_water_area (id),
     CONSTRAINT voyage_load_line_type_fk FOREIGN KEY (load_line_id) REFERENCES load_line_type (id),
     CONSTRAINT wave_length_check CHECK (wave_length > 1.6 AND wave_length < 351.0),
-    CONSTRAINT course_angle_check CHECK (course_angle > 0.0 AND course_angle < 360.0)  
+    CONSTRAINT course_angle_check CHECK (course_angle > 0.0 AND course_angle < 360.0),
+    CONSTRAINT wave_angle_check CHECK (wave_angle > 0.0 AND wave_angle < 360.0),
+    CONSTRAINT current_speed_check CHECK (current_speed > 1.0 AND current_speed < 15.0)
 );
